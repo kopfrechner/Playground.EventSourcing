b992bd334d2fc7879de2999ec5fde663db2245a7
Christoph Kopf <christoph.kopf@objectbay.com>
2025-03-08 23:16:56 +0100

    use random db

--- Playground.EventSourcing.Tests/Common/PostgresTestContainerFixture.cs
+++ Playground.EventSourcing.Tests/Common/PostgresTestContainerFixture.cs
@@ -1,14 +1,17 @@
+using Bogus;
 using Testcontainers.PostgreSql;
 
 namespace Playground.EventSourcing.Tests.Common;
 
 public class PostgresTestContainerFixture : IAsyncLifetime
 {
+    private static Faker _faker = new Faker();
+    
     public PostgreSqlContainer Container { get; } = new PostgreSqlBuilder()
         .WithImage("postgres:latest")
-        .WithDatabase("testdb")
-        .WithUsername("testuser")
-        .WithPassword("testpass")
+        .WithDatabase(_faker.Random.String2(10))
+        .WithUsername(_faker.Random.String2(10))
+        .WithPassword(_faker.Random.String2(10))
         .WithAutoRemove(true)
         .Build();
 
