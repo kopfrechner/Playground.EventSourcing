6240c9796ebf7e14ebf973b4a4e53be8588b8469
Christoph Kopf <christoph.kopf@objectbay.com>
2025-03-08 12:28:08 +0100

    play

--- Playground.EventSourcing/Program.cs
+++ Playground.EventSourcing/Program.cs
@@ -1,16 +1,14 @@
 ﻿using Playground.EventSourcing;
 
-// Load configuration from user secrets
-var connectionString = EventStoreHelper.ConnectionStringOrThrow();
-
 // Configure Marten
+var connectionString = EventStoreHelper.ConnectionStringOrThrow();
 var store = EventStoreHelper.SetupDocumentStore(connectionString);
 
 await using var session = store.LightweightSession();
 
 var orderId = Guid.NewGuid();
 
-// Stream starten mit Events
+// Events
 session.Events.StartStream(orderId,
     new OrderPlaced(orderId, "Laptop", 2),
     new OrderShipped(orderId, DateTime.UtcNow),
@@ -18,9 +16,19 @@ session.Events.StartStream(orderId,
 );
 await session.SaveChangesAsync();
 
-// Aggregate rekonstruieren
+// Manually with FetchStream
 var events = await session.Events.FetchStreamAsync(orderId);
 var order = new OrderAggregate();
 foreach (var e in events) order.ApplyEvent(e.Data);
-
 Console.WriteLine($"Order: {order.ProductName}, Shipped: {order.ShippedDate}, Completed: {order.IsCompleted}");
+
+// Try to get aggregate with marten and versioning
+var orderAggregateV1 = await session.Events.AggregateStreamAsync<OrderAggregate>(orderId, 1);
+Console.WriteLine($"Order: {orderAggregateV1!.ProductName}, Shipped: {orderAggregateV1.ShippedDate}, Completed: {orderAggregateV1.IsCompleted}");
+
+var orderAggregateV2 = await session.Events.AggregateStreamAsync<OrderAggregate>(orderId, 2);
+Console.WriteLine($"Order: {orderAggregateV2!.ProductName}, Shipped: {orderAggregateV2.ShippedDate}, Completed: {orderAggregateV2.IsCompleted}");
+
+var orderAggregate = await session.Events.AggregateStreamAsync<OrderAggregate>(orderId);
+Console.WriteLine($"Order: {orderAggregate!.ProductName}, Shipped: {orderAggregate.ShippedDate}, Completed: {orderAggregate.IsCompleted}");
+
